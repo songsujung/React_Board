9e3901750cecc189417191d23e4e782c4b5e91fd
sj <sujung033131@gmail.com>
2023-07-17 01:10:52 +0900

    0716 React Board list + paging

--- src/components/board/ListComponent.js
+++ src/components/board/ListComponent.js
@@ -14,9 +14,14 @@ const initSate = {
     requestDTO : null
   }
 
-const ListComponent = ({queryObj}) => {
+const ListComponent = ({queryObj, movePage}) => {
 
     const [listData, setListData] = useState(initSate) // 초기값 설정
+
+    // page click event
+    const handleClickPage = (pageNum) =>{
+        movePage(pageNum)
+    }
 
     // useEffect 사용하여, queryObj의 변화를 감지하고, 그에 따라 getList 함수를 호출
     // queryObj가 변경될 때마다 데이터를 업데이트
@@ -39,6 +44,32 @@ const ListComponent = ({queryObj}) => {
             <ul>
               {/* dto를 {bno , title , writer , replyCount} 구조분해 할당해서 사용하면 편리함 */}
               {listData.dtoList.map(({bno , title , writer , replyCount}) => <li key={bno}>{bno} - {title} - {writer}</li>)}
+            </ul>
+          </div>
+
+          {/* 페이징 처리 */}
+          <div className="flex m-4 p-2">
+            <ul className="flex">
+                {listData.prev ? 
+                <li className="m-2 p-2 bg-blue-500 text-white font-extrabold underline"
+                onClick={()=>handleClickPage(listData.start - 1)}
+                >PREV
+                </li> : <></>}
+
+            {listData.pageNums.map(num => 
+            <li 
+            key={num} 
+            className="m-2 p-2 bg-blue-500 text-white font-extrabold underline"
+            onClick={() => handleClickPage(num)}
+            >{num}
+            </li>)}
+
+            {listData.next ? 
+            <li className="m-2 p-2 bg-blue-500 text-white font-extrabold underline"
+            onClick={()=>handleClickPage(listData.end + 1)}
+            >NEXT
+            </li> : <></>}
+
             </ul>
           </div>
         </div>
--- src/pages/board/ListPage.js
+++ src/pages/board/ListPage.js
@@ -2,6 +2,25 @@ import { useSearchParams } from "react-router-dom";
 import BasicLayout from "../../layouts/BasicLayout";
 import ListComponent from "../../components/board/ListComponent";
 
+const checkNull = (obj) => {
+
+    // 새로운 빈 객체 result를 생성
+    const result = {}
+  
+    // for문으로 obj 객체의 속성들을 하나씩 차례대로 탐색
+    // 속성 : 객체에 포함된 변수 또는 값의 이름( 키(key)와 값(value)의 쌍으로 구성 )
+    for (const attr in obj) {
+      const attrName = attr // 속성의 이름을 attrName에 할당
+      const attrValue = obj[attr] // 속성의 값 attrValue에 할당
+  
+      if( attrValue && attrValue !== 'null'){
+        result[attrName] = attrValue // 조건식이 만족하는 경우 속성과 속성값을 result에 저장
+      }
+    }
+  
+    return result
+  }
+
 const ListPage = () => {
 
     const [search, setSearch] = useSearchParams()
@@ -15,7 +34,19 @@ const ListPage = () => {
 
     const keyword = search.get("keyword")
 
-    const queryObj = {page, size, type, keyword}
+    const queryObj = checkNull({page, size, type, keyword})
+
+    // 페이지 번호를 누르면 URL이 변경
+    const movePage = (num) => {
+        console.log("NUM--------" + num)
+        
+        // queryObj객체의 page속성(값)을 num으로 변경
+        queryObj.page = num 
+
+        // queryObj객체를 변경하여 컴포넌트의 상태를 업데이트 
+        // {...} <- 전개연산자 : 배열이나 객체를 펼쳐서 개별 요소로 분리하거나, 여러 개의 배열이나 객체를 결합할 때 사용
+        setSearch({...queryObj})
+    }
 
     console.log("page: " + page)
     console.log("size: " + size)
@@ -31,7 +62,7 @@ const ListPage = () => {
 
     return ( 
         <div>
-            Board List Page <ListComponent queryObj={queryObj}></ListComponent>
+            Board List Page <ListComponent queryObj={queryObj} movePage={movePage}></ListComponent>
         </div>
      );
 }
